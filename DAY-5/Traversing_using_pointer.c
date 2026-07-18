bca2018ac6b0c1260a71567457a0652772d41871
aashi-tech-coder <aashivarshney14@gmail.com>
2026-07-18 22:33:46 +0530

    Implement array traversal using pointers
    Fix pointer dereference syntax in array traversal.

---
+++ DAY-5/Traversing_using_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int arr[3] ={10,20,30};
+   int *p = arr;
+   
+   printf("Array traversing using pointers:\n");
+   for (int i = 0; i<3; i++){
+       printf("%d\n", *(p+i));
+   }
+       return 0;
+}
